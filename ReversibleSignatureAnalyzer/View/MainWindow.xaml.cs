116e5a1f4d2de0022e1edad8e677d06a48cee74f
MatCoder77 <236382@student.pwr.edu.pl>
2020-12-17 11:43:31 +0100

    Improved UX

--- ReversibleSignatureAnalyzer/View/MainWindow.xaml.cs
+++ ReversibleSignatureAnalyzer/View/MainWindow.xaml.cs
@@ -32,7 +32,6 @@ namespace ReversibleSignatureAnalyzer.View
         private bool isFileLoaded = true;
         private BitmapImage importedImage;
         private BitmapImage resultImage;
-        private string activityType;
         private AlgorithmConfiguration currentEncodingDeConfiguration = new DifferencesExpansionConfiguration(20, Direction.Horizontal, new HashSet<EmbeddingChanel>(){ EmbeddingChanel.R });
         private AlgorithmConfiguration currentDecodingDeConfiguration = new DifferencesExpansionConfiguration(20, Direction.Horizontal, new HashSet<EmbeddingChanel>() { EmbeddingChanel.R });
         private AlgorithmConfiguration currentEncodingHsConfiguration = new HistogramShiftingConfiguration(false, new HashSet<EmbeddingChanel>() { EmbeddingChanel.R });
@@ -45,14 +44,7 @@ namespace ReversibleSignatureAnalyzer.View
         public MainWindow()
         {
             InitializeComponent();
-            HideStartUp();
             SetStartup();
-        }
-
-        private void HideStartUp()
-        {
-            BtnExportFile.Visibility = Visibility.Collapsed;
-            TvExportFileName.Visibility = Visibility.Collapsed;
         }
 
         private void SetStartup()
@@ -60,9 +52,7 @@ namespace ReversibleSignatureAnalyzer.View
             RbAlgorithm1.IsChecked = true;
             CbActivityType.SelectedIndex = 0;
             addSignatureController = new AddSignatureController();
-            RbAlgorithm1.IsChecked = true;
             ImportImage(projectDirectory + "/Model/_img/lena.png");
-            activityType = CbActivityType.Text;
         }
 
         private void BtnImportFile_Click(object sender, RoutedEventArgs e)
@@ -85,7 +75,7 @@ namespace ReversibleSignatureAnalyzer.View
             importedImage = new BitmapImage(new Uri(fileName));
             ImgImport.Source = importedImage;
             ImgExport.Source = null;
-            SetTextRichTextBox(TvPayload, "Place for payload");
+            SetTextRichTextBox(TvPayload, "Type in your payload");
             TvExportFileName.Text = "";
         }
 
@@ -104,6 +94,7 @@ namespace ReversibleSignatureAnalyzer.View
                 Tuple<BitmapImage, string> imageAndPayload = addSignatureController.GetDecodedImage(importedImage, selectedAlgorithm, GetDecodingConfigurationForSelectedAlgorithm());
                 resultImage = imageAndPayload.Item1;
                 SetTextRichTextBox(TvPayload, imageAndPayload.Item2);
+                TvPayload.Visibility = Visibility.Visible;
             }
 
             ImgExport.Source = resultImage;
@@ -208,12 +199,14 @@ namespace ReversibleSignatureAnalyzer.View
 
         private void OperationSuccessNotify(string msg)
         {
+            TvOperationResult.Visibility = Visibility.Visible;
             TvOperationResult.Text = $"{SUCCESS} {msg}";
             TvOperationResult.Foreground = Brushes.ForestGreen;
         }
 
         private void OperationErrorNotify(string msg)
         {
+            TvOperationResult.Visibility = Visibility.Visible;
             TvOperationResult.Text = msg;
             TvOperationResult.Foreground = Brushes.Red;
         }
@@ -378,6 +371,28 @@ namespace ReversibleSignatureAnalyzer.View
         private void TvPayload_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void CbActivityType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CbActivityType.SelectedItem != null)
+            {
+                string selection = (e.AddedItems[0] as ComboBoxItem).Content as string;
+                if (selection == TbAdd.Content.ToString())
+                {
+                    SetTextRichTextBox(TvPayload, "Type in your payload");
+                    TvPayload.Visibility = Visibility.Visible;
+                }
+                if (selection == TbAnalyze.Content.ToString())
+                {
+                    SetTextRichTextBox(TvPayload, "");
+                    TvPayload.Visibility = Visibility.Collapsed;
+                }
+                TvOperationResult.Visibility = Visibility.Collapsed;
+                BtnExportFile.Visibility = Visibility.Collapsed;
+                TvExportFileName.Visibility = Visibility.Collapsed;
+                TvExportFileName.Text = "";
+            }
         }
     }
 }
